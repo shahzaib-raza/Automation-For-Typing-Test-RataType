02c91e5dcc2ada1b95e504de1d8943cc2d5cff65
Shahzaib Raza <64272560+shahzaib-raza@users.noreply.github.com>
2020-08-31 10:59:05 +0500

    comments aded

--- Auto Typing Test.py
+++ Auto Typing Test.py
@@ -1,30 +1,37 @@
+# imports required
+
 import selenium
 from selenium.webdriver.common.keys import Keys
 import time
 
-dpath = input('Enter path of driver: ')
+# inputs required
+
+dpath = input('Enter path of chrome_driver: ')
 email = input('Enter the login Email of Ratatype: ')
 paswd = input('Enter the password of Ratatype: ')
-print('For 40 wpm speed enter time of delay 0.27 sec')
+print('For 40 wpm speed enter time delay of 0.27 sec')
 t = float(input('Enter time delay: '))
+
+# chrome driver gets the Ratatype test web_page
 
 driver = selenium.webdriver.Chrome(executable_path=dpath)
 driver.get('https://www.ratatype.com/typing-test/')
 time.sleep(2)
 
+# driver login user at Ratatype website
+
 log = driver.find_element_by_xpath('/html/body/header/div/nav/div[2]/ul[2]/li/a[1]')
 log.click()
 mail = driver.find_element_by_id('email')
 mail.send_keys(email)
-password = driver.find_element_by_id(paswd)
-password.send_keys('austin881')
+password = driver.find_element_by_id('password')
+password.send_keys(paswd)
 driver.find_element_by_xpath('/html/body/section/div/div/div/div/form/div[3]/button').click()
-
 time.sleep(2)
-
 driver.find_element_by_xpath('/html/body/div[2]/nav/ul/li[3]/a').click()
-
 time.sleep(2)
+
+# driver will start the typing test
 
 button = driver.find_element_by_xpath('/html/body/section/div[1]/div/div/div/div/a')
 button.click()
@@ -39,3 +46,4 @@ for word in main_text:
         send.send_keys(letter)
         time.sleep(t)
 
+print('Task completed print your certificate from the automated window.')
